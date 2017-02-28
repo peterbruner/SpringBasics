cf16f5acc5b70a0aeb0f55fa3cd93667b368469d
Steve Jobs <peter@steves-mbp.nvstu.edu>
2017-02-28 17:24:28 -0500

    first four bullet points

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -1,8 +1,42 @@
 package com.theironyard.novauc;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
+    static User user;
+
     public static void main(String[] args) {
-	// write your code here
+
+        Spark.init();
+
+        Spark.get("/", ((request, response) -> {
+            HashMap hashBrowns = new HashMap();
+            if(user == null) {
+                return new ModelAndView(hashBrowns, "index.html");
+            } else {
+                hashBrowns.put("name", user.getName());
+                return new ModelAndView(hashBrowns, "messages.html");
+            }
+
+        }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post("/login", ((request, response) -> {
+            String name = request.queryParams("loginName");
+
+            user = new User(name);
+            response.redirect("/");
+            return "";
+            })
+
+        );
+
+        //Spark.post("/create-user");
     }
 }
